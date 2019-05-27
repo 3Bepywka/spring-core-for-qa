b09ce460736f8e7d67a7be5bd8b3eb9f22fb50b5
Ekaterina Zvereva <ekaterinavzvereva@gmail.com>
2019-05-27 21:57:59 +0300

    Edit oop unit tests to spring unit tests. Add tests for 100% coverage.

--- src/test/java/com/acme/banking/dbo/ooad/ReportingServiceTest.java
+++ src/test/java/com/acme/banking/dbo/ooad/ReportingServiceTest.java
@@ -1,74 +1,121 @@
 package com.acme.banking.dbo.ooad;
 
-import com.acme.banking.dbo.ooad.dal.AccountRepository;
-import com.acme.banking.dbo.ooad.domain.Account;
-import com.acme.banking.dbo.ooad.service.ReportingService;
+import com.acme.banking.dbo.spring.dao.AccountRepository;
+import com.acme.banking.dbo.spring.domain.Account;
+import com.acme.banking.dbo.spring.domain.CheckingAccount;
+import com.acme.banking.dbo.spring.domain.SavingAccount;
+import com.acme.banking.dbo.spring.service.ReportingService;
 import org.junit.Test;
-
-import javax.persistence.EntityNotFoundException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.util.Optional;
 
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration({"classpath:test-spring-context.xml", "classpath:spring-context.xml"})
+@ActiveProfiles("test")
 public class ReportingServiceTest {
 
-    @Test(expected = EntityNotFoundException.class)
-    public void shouldGetExceptionWhenNoAccountFound() {
-        AccountRepository stubAccountRepository = mock(AccountRepository.class);
-        when(stubAccountRepository.findById(anyLong()))
-                .thenThrow(new EntityNotFoundException());
+    @Autowired
+    private ReportingService reportingService;
+    @MockBean
+    private AccountRepository accounts;
 
-        ReportingService sut = new ReportingService(stubAccountRepository);
-        sut.reportForAccount(0L);
+
+    @Test(expected = IllegalStateException.class)
+    public void shouldGetExceptionWhenNoAccountFound() {
+        reportingService.accountReport(0L);
     }
 
     @Test
     public void shouldGetReportForAccountWhenAccountExists() {
         //region Given
         Account stubAccount = mock(Account.class);
-        when(stubAccount.getAmount()).thenReturn(100.0);
-        when(stubAccount.getId()).thenReturn(0L);
-
-        AccountRepository stubRepo = mock(AccountRepository.class);
-        when(stubRepo.findById(anyLong())).thenReturn(stubAccount);
-        ReportingService sut = new ReportingService(stubRepo);
+        when(stubAccount.toString()).thenReturn("123 150.0");
+        when(accounts.findById(anyLong())).thenReturn(Optional.of(stubAccount));
         //endregion
 
         //region When
-        String actualReport = sut.reportForAccount(0L);
+        String actualReport = reportingService.accountReport(123L);
         //endregion
 
         //region Then
         assertThat(actualReport)
-                .contains("##")
-                .contains(String.valueOf(0L))
-                .contains("100.0");
+                .contains(String.valueOf(123L))
+                .contains("150.0");
         //endregion
     }
 
     @Test
-    public void shouldSetAccount() {
+    public void shouldReturnDefaultLayoutMarker() {
         //region Given
         Account stubAccount = mock(Account.class);
-        when(stubAccount.getAmount()).thenReturn(100.0);
-        when(stubAccount.getId()).thenReturn(1L);
-
-        AccountRepository stubRepo = mock(AccountRepository.class);
-        when(stubRepo.findById(1L)).thenReturn(stubAccount);
-
-        ReportingService sut = new ReportingService(stubRepo);
+        when(stubAccount.toString()).thenReturn("123 150.0");
+        when(accounts.findById(anyLong())).thenReturn(Optional.of(stubAccount));
         //endregion
 
         //region When
-        sut.setAccounts(stubRepo);
+        String actualReport = reportingService.accountReport(123L);
         //endregion
 
         //region Then
-        assertThat(sut.reportForAccount(1L))
-                .contains("100")
-                .contains(String.valueOf(1L));
+        assertThat(actualReport)
+                .contains("##");
         //endregion
+    }
+
+    @DirtiesContext
+    @Test
+    public void shouldReturnSetLayoutMarker() {
+        //region Given
+        Account stubAccount = mock(Account.class);
+        when(stubAccount.toString()).thenReturn("123 150.0");
+        when(accounts.findById(anyLong())).thenReturn(Optional.of(stubAccount));
+        reportingService.setLayoutMarker(">");
+        //endregion
+
+        //region When
+        String actualReport = reportingService.accountReport(123L);
+        //endregion
+
+        //region Then
+        assertThat(actualReport)
+                .contains(">>");
+        //endregion
+    }
+
+    @Test
+    public void shouldReturnCheckingAccountInfoWhenToString() {
+        CheckingAccount account = new CheckingAccount(100, 5, "email@email.com");
+
+        assertTrue(account.toString().equals("0 100.0 C 5.0"));
+    }
+
+    @Test
+    public void shouldReturnSavingAccountInfoWhenToString() {
+        SavingAccount account = new SavingAccount(150,"email@email.com");
+
+        assertTrue(account.toString().equals("0 150.0 S"));
+    }
+
+    @Test
+    public void shouldChangeAccountAmountWhenSetAmount() {
+        CheckingAccount account = new CheckingAccount(100, 5, "email@email.com");
+
+        assertTrue(account.toString().equals("0 100.0 C 5.0"));
+
+        account.setAmount(150);
+
+        assertTrue(account.toString().equals("0 150.0 C 5.0"));
     }
 }
--- src/test/java/com/acme/banking/dbo/ooad/TransferServiceTest.java
+++ src/test/java/com/acme/banking/dbo/ooad/TransferServiceTest.java
@@ -1,28 +1,86 @@
 package com.acme.banking.dbo.ooad;
 
-import com.acme.banking.dbo.ooad.domain.Account;
-import com.acme.banking.dbo.ooad.service.TransferService;
+import com.acme.banking.dbo.spring.dao.AccountRepository;
+import com.acme.banking.dbo.spring.domain.Account;
+import com.acme.banking.dbo.spring.service.TransferService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration({"classpath:test-spring-context.xml", "classpath:spring-context.xml"})
+@ActiveProfiles("test")
 public class TransferServiceTest {
+    @Autowired
+    private TransferService transferService;
+    @MockBean
+    private AccountRepository accounts;
 
     @Test
     public void shouldUpdateAccountsStateWhenTransfer() {
         //region Given
-        TransferService sut = new TransferService();
-        Account fromAccount = mock(Account.class);
-        Account toAccount = mock(Account.class);
+        Account fromAccountStub = mock(Account.class);
+        when(fromAccountStub.getAmount()).thenReturn(100d);
+        when(accounts.findById(0L)).thenReturn(Optional.of(fromAccountStub));
+        Account toAccountStub = mock(Account.class);
+        when(toAccountStub.getAmount()).thenReturn(0d);
+        when(accounts.findById(1L)).thenReturn(Optional.of(toAccountStub));
         //endregion
 
         //region When
-        sut.transfer(fromAccount, toAccount, 100.);
+        transferService.transfer(0L, 1L, 100);
         //endregion
 
         //region Then
-        verify(fromAccount, times(1)).withdraw(100.);
-        verify(toAccount).deposit(anyDouble());
+        verify(fromAccountStub, times(1)).setAmount(0d);
+        verify(toAccountStub).setAmount(100d);
+        //endregion
+    }
+
+    @Test
+    public void shouldFindByIdForAccountsWhenTransfer() {
+        //region Given
+        Account fromAccountStub = mock(Account.class);
+        when(fromAccountStub.getAmount()).thenReturn(100d);
+        when(accounts.findById(0L)).thenReturn(Optional.of(fromAccountStub));
+        Account toAccountStub = mock(Account.class);
+        when(toAccountStub.getAmount()).thenReturn(0d);
+        when(accounts.findById(1L)).thenReturn(Optional.of(toAccountStub));
+        //endregion
+
+        //region When
+        transferService.transfer(0L, 1L, 100);
+        //endregion
+
+        //region Then
+        verify(accounts, times(1)).findById(0L);
+        verify(accounts, times(1)).findById(1L);
+        //endregion
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void shouldReturnExceptionWhenFirstAccountNotFoundWhenTransfer() {
+        //region When
+        transferService.transfer(0L, 1L, 100);
+        //endregion
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void shouldReturnExceptionWhenSecondAccountNotFoundWhenTransfer() {
+        //region Given
+        Account fromAccountStub = mock(Account.class);
+        when(fromAccountStub.getAmount()).thenReturn(100d);
+        when(accounts.findById(0L)).thenReturn(Optional.of(fromAccountStub));
+        //endregion
+        //region When
+        transferService.transfer(0L, 1L, 100);
         //endregion
     }
 }
